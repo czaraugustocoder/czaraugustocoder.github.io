f1393ef665242c9c85d4dd1fec24aeabfeaf4663
Cesar Augusto <108474749+czaraugustocoder@users.noreply.github.com>
2024-10-28 11:36:38 -0300

    Delete ARTICLES directory diretorio deletado

--- ARTICLES/PROTOTIPO-OSPAM/crime.js
+++
@@ -1,8 +0,0 @@
-const btnMobile = document.getElementById("btn-mobile");
-
-function openMenu() {
-    const nav = document.getElementById("nav-menu");
-    nav.classList.toggle('active');
-}
-
-btnMobile.addEventListener('click', openMenu);
